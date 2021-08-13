82655d4f8ef031daaddb500a578ecc85a5e20ce0
Clint Edwards <cedwards1080@gmail.com>
2021-08-12 19:58:45 -0600

    create-view redo

---
+++ redo/create-view.sql
@@ -0,0 +1,26 @@
+CREATE VIEW rock AS
+SELECT * 
+FROM track
+WHERE genre_id IN (
+    SELECT genre_id
+    FROM genre
+    WHERE name = 'Rock'
+);
+SELECT * 
+FROM Rock;
+
+CREATE VIEW classical_count AS
+SELECT COUNT(*)
+FROM track
+WHERE track_id IN (
+    SELECT track_id
+    FROM playlist_track
+    WHERE playlist_id IN (
+        SELECT playlist_id
+        FROM playlist
+        WHERE name = 'Classical'
+    )
+);
+
+SELECT * 
+FROM classical_count;
